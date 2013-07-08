bbaa9cc3a0821969791b58cd6293384221e646d1
Bruno Oliveira <bruni.nho_oliveira@hotmail.com>
2013-07-07 23:27:27 -0300

    DB Sys Control Collection - Criação do Banco

---
+++ DB_ControlCollection.sql
@@ -0,0 +1,18 @@
+-- Autor: Bruno Oliveira
+-- Data.: 07 de Julho de 2013
+
+-- Banco de dados do Sistema de controle de Acervo pessoal de
+-- Filmes, Jogos e livros - Sys Control Collection
+
+-- ---------------------------------------------------------------- --
+--                  Criação do Banco de dados                       --
+-- ---------------------------------------------------------------- --
+CREATE DATABASE DB_ControlCollection
+GO
+
+USE DB_ControlCollection
+GO
+
+-- ---------------------------------------------------------------- --
+--                    Criação das Tabelas                           --
+-- ---------------------------------------------------------------- --
